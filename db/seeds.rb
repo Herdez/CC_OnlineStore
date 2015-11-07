0eb755d2efd5d01b230f5454f241c5fc0a86b197
codeamac01 <codeamac01@gmail.com>
2015-11-06 19:19:45 -0600

    online store with ActiveRecord

---
+++ db/seeds.rb
@@ -0,0 +1,39 @@
+# Este archivo sirve para crear registros de prueba# Este archivo sirve para crear registros de prueba
+module UserSeeds
+
+  def self.import(filename=File.dirname(__FILE__) + "/../users.csv")
+    field_names = nil
+    User.transaction do
+      File.open(filename).each do |line|
+        data = line.chomp.split(',')
+        if field_names.nil?
+          field_names = data
+        else
+          attribute_hash = Hash[field_names.zip(data)]
+          p attribute_hash
+          User.create!(attribute_hash)
+        end
+      end
+    end
+  end
+end
+
+
+module ProductSeeds
+
+  def self.import(filename=File.dirname(__FILE__) + "/../products.csv")
+    field_names = nil
+    Product.transaction do
+      File.open(filename).each do |line|
+        data = line.chomp.split(',')
+        if field_names.nil?
+          field_names = data
+        else
+          attribute_hash = Hash[field_names.zip(data)]
+          p attribute_hash
+          Product.create!(attribute_hash)
+        end
+      end
+    end
+  end
+end
